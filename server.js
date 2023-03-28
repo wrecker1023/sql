7219001b5301d6aeaf44efbc0baabfa638143a33
Wrecker1023 <wrecker1023@gmail.com>
2023-03-28 19:56:25 -0400

    added table data to express server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,6 +11,23 @@ app.use(express.json());
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
+});
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'aiden',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 app.listen(PORT, () => {
