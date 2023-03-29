29840a5a9088f48ddbfb213f38654401b97b648d
Wrecker1023 <wrecker1023@gmail.com>
2023-03-28 20:44:27 -0400

    added delete request

--- server.js
+++ server.js
@@ -55,6 +55,28 @@ app.get('/api/candidate/:id', (req,res) => {
     });
 });
 
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
