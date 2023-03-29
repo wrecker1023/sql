c3e024ec687a837e4c40863aed1af47006c35c20
Wrecker1023 <wrecker1023@gmail.com>
2023-03-28 20:50:47 -0400

    added CRUD routes

--- server.js
+++ server.js
@@ -77,6 +77,29 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 });
 
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'success',
+    data: body
+  });
+});
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
